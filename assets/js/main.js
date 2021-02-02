fe03c32f0b2f50de9de039542d6177e6fb9303cc
Thomas Dehaeze <dehaeze.thomas@gmail.com>
2021-02-02 13:49:44 +0100

    Add boxes, fonts and mathjax scss

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,33 @@ $(document).ready(function () {
   Even.backToTop();
   Even.mobileNavbar();
   Even.toc();
+
+  // Blocks
+  var boxesNames = {
+    "seealso": "See also",
+    "definition": "Definition",
+    "exampl": "Example",
+    "exercice": "Exercice",
+    "question": "Question",
+    "summary": "Summary",
+    "note": "Note",
+    "answer": "Answer",
+    "hint": "Hint",
+    "important": "Important",
+    "caution": "Caution",
+    "warning": "Warning"
+  };
+  for (var className in boxesNames) {
+    let boxes = document.querySelectorAll('div.' + className);
+
+    for (let box of boxes) {
+      let boxTitle = document.createElement('p');
+      boxTitle.className = "admonition-title " + className;
+      boxTitle.textContent = boxesNames[className];
+
+      box.parentNode.insertBefore(boxTitle, box);
+    }
+  }
 });
 
 Even.responsiveTable();
@@ -14,4 +41,3 @@ if (window.hljs) {
 } else {
   Even.chroma();
 }
-
